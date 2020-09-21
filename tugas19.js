afdddffb5d98fc9fe2a654962c3a4318d6c7d174
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-21 11:53:03 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+var kata = 'Semangat...'
+var ulang = kata.repeat(5)
+
+console.log(ulang)
+
+var star = '*****'
+console.log(star.repeat(1))
+console.log(star.repeat(3))
+console.log(star.repeat(5))
+console.log(star.repeat(7))
+console.log(star.repeat(9))
+console.log(star.repeat(7))
+console.log(star.repeat(5))
+console.log(star.repeat(3))
+console.log(star.repeat(1))
+
